9f4eb6679d6f9c24e4b65376cf265e886d90193c
Marta <159387209+martaic@users.noreply.github.com>
2025-06-10 09:34:30 +0000

    merge

--- src/front/components/ocio.jsx
+++ src/front/components/ocio.jsx
@@ -1,12 +1,22 @@
 import "../styles/Ocio.css";
 
-export  const Ocio = () => {
-    return(
+export const Ocio = () => {
+    return (
         <div className="container-ocio">
+            <nav class="navbar bg-body-tertiary">
+                <div class="container-fluid">
+                    <a class="navbar-brand">Busqueda</a>
+                    <form class="d-flex" role="search">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                        <button class="btn btn-outline-success" type="submit">Search</button>
+                    </form>
+                </div>
+            </nav>
             <h1 className="text-center my-5">Ocio</h1>
             <p className="text-center">Aquí puedes gestionar tus actividades de ocio.</p>
             <div className="text-center">
                 <button className="btn btn-primary">Añadir actividad</button>
             </div>
         </div>
-    )}
+    )
+}
