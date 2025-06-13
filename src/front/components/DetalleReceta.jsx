c49d73918c2c33a4e227ddd41df8a71fcb541cbd
timo131 <thomasemosley3@gmail.com>
2025-06-13 19:54:22 +0000

    ingredients and instructions added

--- src/front/components/DetalleReceta.jsx
+++ src/front/components/DetalleReceta.jsx
@@ -20,12 +20,16 @@ export const DetalleReceta = () => {
       <p>{receta.cuisines?.[0]}</p>
       <p><strong>Porciones:</strong> {receta.servings}</p>
       <p><strong>Listo en:</strong> {receta.readyInMinutes} min.</p>
+    <p><strong>Ingredientes:</strong></p>
+        <ul>
+          {receta.extendedIngredients.map((ingredient) => (
+            <li key={ingredient.id || ingredient.name}>
+              {ingredient.original}
+            </li>
+          ))}
+        </ul>
       <p><strong>Valor nutricional:</strong> {receta.healthScore} %</p>
-          <strong>Descripción:</strong><p
-              dangerouslySetInnerHTML={{
-                  __html: DOMPurify.sanitize(receta.summary)
-              }}
-          />
+      <p><strong>Instrucciones:</strong> {receta.instructions}</p>
           {receta.image && receta.image !== "" && (
         <img src={receta.image} alt={receta.title} className="img-fluid mt-3" />
       )}
