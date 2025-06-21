01e1fe9c3dfd901024be19341c69c2696aa20bd9
Marta <159387209+martaic@users.noreply.github.com>
2025-06-21 18:37:22 +0000

    tareaHogar

--- src/front/components/tareas.jsx
+++ src/front/components/tareas.jsx
@@ -8,6 +8,10 @@ export const Tareas = () => {
 	const navigate = useNavigate();
 
 	const [filtro, setFiltro] = useState("todas");
+<<<<<<< HEAD
+=======
+gi
+>>>>>>> 933a44b (tareaHogar)
 	const handleRedirect = () => {
 		navigate("/card");
 	};
