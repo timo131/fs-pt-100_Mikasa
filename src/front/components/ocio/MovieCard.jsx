5210bef3ad588cdeedfac0fda239ca8fca3ec71e
timo131 <thomasemosley3@gmail.com>
2025-06-24 17:16:21 +0000

    better

--- src/front/components/ocio/MovieCard.jsx
+++ src/front/components/ocio/MovieCard.jsx
@@ -164,9 +164,9 @@ export const MovieCard = ({ id }) => {
                                     onClick={() => setShowRating(true)}
                                     title="Me gusta"
                                 >
-                                    <span className={`fa-${isLiked ? "solid" : "regular"} fa-thumbs-up fa-2x
-                                    ${currentRating <= 0 ? "ochre" : "sage"}`}
-                                    ></span>
+                                    <span className={`fa-${isLiked ? "solid" : "regular"} fa-thumbs-up fa-2x ${
+                                    !isLiked ? "charcoal" : currentRating <= 0 ? "ochre" : "aqua"
+                                    }`}></span>
                                 </button>
                                 <button className="btn btn-sm" onClick={toggleFavorite} title="Favorito">
                                     <span className={`fa fa-${isFavorite ? "solid" : "regular"} fa-heart fa-2x coral`}></span>
